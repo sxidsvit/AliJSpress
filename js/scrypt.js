83262830e6a1192703d0f46dfdfc508283156cde
unknown <s.p.antonyuk@gmail.com>
2020-01-17 11:09:27 +0200

    fix: корректировка id для товаров

--- js/scrypt.js
+++ js/scrypt.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
+    // Поиск, кнопка корзины, список желаний, обертка товаров
     const search = document.querySelectorAll('.search');
-    console.log('search', search);
     const cartBtn = document.getElementById('cart');
     const wishlistBtn = document.getElementById('wishlist');
     const goodsWrapper = document.querySelector('.goods-wrapper');
+    // Модальное окно с карзиной
     const cart = document.querySelector('.cart');
 
+    // Создаем карточку из её шаблона
     const createCardGoods = (id, title, price, img) => {
         const card = document.createElement('div');
         card.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
@@ -27,30 +29,31 @@ document.addEventListener('DOMContentLoaded', () => {
         return card;
     }
     goodsWrapper.appendChild(createCardGoods(1, 'Darts', 2000, 'img/temp/Archer.jpg'));
-    goodsWrapper.appendChild(createCardGoods(1, 'Flamingo', 6000, 'img/temp/Flamingo.jpg'));
-    goodsWrapper.appendChild(createCardGoods(1, 'Noski', 300, 'img/temp/Socks.jpg'));
+    goodsWrapper.appendChild(createCardGoods(2, 'Flamingo', 6000, 'img/temp/Flamingo.jpg'));
+    goodsWrapper.appendChild(createCardGoods(3, 'Noski', 300, 'img/temp/Socks.jpg'));
 
 
 
-    const openCart = () => {
+    const openCart = (event) => {
         event.preventDefault();
         cart.style.display = 'flex';
         return false;
 
     }
 
-    const closeCart = () => {
+    const closeCart = (event) => {
         const target = event.target;
         console.log('event: ', event);
         const key = event.keyCode;
         if (target.classList.contains('cart') || target.classList.contains('cart-close') || key == 27 || key == 32) {
             cart.style.display = '';
         }
+        document.removeEventListener('keyup', closeCart)
 
     }
 
     cartBtn.addEventListener('click', openCart);
     cart.addEventListener('click', closeCart);
-    window.addEventListener('keydown', closeCart);
+    document.addEventListener('keyup', closeCart);
 
 })
